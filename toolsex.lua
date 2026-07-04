082ba29ea4985dd481d0362503b661479bb08d27
Banv <tokaruunhi@gmail.com>
2026-07-04 16:53:58 +0700

    Update toolsex.lua

--- toolsex.lua
+++ toolsex.lua
@@ -1,22 +1,11 @@
-task.spawn(function()
- while task.wait(10) do
-collectgarbage('collect')
-end
-end)
+local TeleportService = game:GetService("TeleportService")
 
-		game:GetService("TeleportService").TeleportInitFailed:Connect(function(player,result,err)
-    		if result == Enum.TeleportResult.GameFull then
-    		end
-			end)
-game:GetService("CoreGui").RobloxPromptGui.promptOverlay.ChildAdded:Connect(function(child)
-    if child.Name == 'ErrorPrompt' and child:FindFirstChild('MessageArea') and child.MessageArea:FindFirstChild("ErrorFrame") then
-      game:GetService("TeleportService").TeleportInitFailed:Connect(function(player,result,err)
-    		if result == Enum.TeleportResult.GameFull then
-            print("Full Nguoi hehehe")
-          else 
-            game:GetService("ReplicatedStorage"):WaitForChild("__ServerBrowser"):InvokeServer("teleport", game.JobId)
-    		end
-          
-			end)
+TeleportService.TeleportInitFailed:Connect(function(player, result, err)
+    if result == Enum.TeleportResult.GameFull then
+        print("Full người hehehe")
+    else
+        game:GetService("ReplicatedStorage")
+            :WaitForChild("__ServerBrowser")
+            :InvokeServer("teleport", game.JobId)
     end
 end)
